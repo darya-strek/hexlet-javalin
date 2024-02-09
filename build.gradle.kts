89ea0a3cbbee85e5ea41667529b1a290acad848e
darya-strekalovskaya <darya.strek@gmail.com>
2024-02-09 04:41:07 +0300

    Add files and classes for Lesson 7

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,13 +21,24 @@ dependencies {
     // Версии зависимостей могут отличаться
     // Здесь мы сразу подключаем зависимости,
     // которые понадобятся во время обучения
+    compileOnly ("org.projectlombok:lombok:1.18.30")
+    annotationProcessor ("org.projectlombok:lombok:1.18.30")
+
+    testCompileOnly ("org.projectlombok:lombok:1.18.30")
+    testAnnotationProcessor ("org.projectlombok:lombok:1.18.30")
+
+    implementation("gg.jte:jte:3.0.1")
+    implementation("io.javalin:javalin-rendering:5.6.2")
     implementation("io.javalin:javalin:5.6.1")
     implementation("org.slf4j:slf4j-simple:2.0.7")
     implementation("io.javalin:javalin-rendering:5.6.0")
     implementation("gg.jte:jte:3.0.1")
+    implementation("org.projectlombok:lombok:1.18.22")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
+
+
 
 tasks.test {
     useJUnitPlatform()
--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -2,15 +2,43 @@ package org.example.hexlet;
 
 import io.javalin.Javalin;
 
+import java.util.Collections;
+
+import org.example.hexlet.dto.courses.CoursesPage;
+import org.example.hexlet.model.Course;
+import org.example.hexlet.dto.courses.CoursePage;
+import org.example.hexlet.repository.CourseRepository;
+
 public class HelloWorld {
+
     public static void main(String[] args) {
+
+        Course php = new Course("php", "it's php");
+        CourseRepository.save(php);
+        Course java = new Course("java", "it's java");
+        CourseRepository.save(java);
+        Course python = new Course("python", "it's python");
+        CourseRepository.save(python);
+
         // Создаем приложение
         var app = Javalin.create(config -> {
             config.plugins.enableDevLogging();
         });
-        // Описываем, что загрузится по адресу /
-        app.get("/users", ctx -> ctx.result("GET /users"));
-        app.post("/users", ctx -> ctx.result("POST /users"));
+
+        app.get("/courses/{id}", ctx -> {
+            var id = ctx.pathParamAsClass("id", Long.class).get();
+            var course = CourseRepository.find(id).get();
+            var page = new CoursePage(course);
+            ctx.render("courses/show.jte", Collections.singletonMap("page", page));
+        });
+
+        app.get("/courses", ctx -> {
+            var courses = CourseRepository.getEntities();
+            var header = "Все курсы по программированию";
+            var page = new CoursesPage(courses, header);
+            ctx.render("courses/index.jte", Collections.singletonMap("page", page));
+        });
+
         app.start(7070); // Стартуем веб-сервер
     }
 }
