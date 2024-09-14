11f7b8e4e3f46abb8f1a78891a0c11523cb71130
darya-strekalovskaya <darya.strek@gmail.com>
2024-09-14 03:32:42 +0300

    Fix HelloWorld.java according lesson 6

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -1,6 +1,8 @@
 package org.example.hexlet;
 
 import io.javalin.Javalin;
+import io.javalin.http.Context;
+import io.javalin.http.NotFoundResponse;
 
 public class HelloWorld {
     public static void main(String[] args) {
@@ -8,15 +10,42 @@ public class HelloWorld {
             config.bundledPlugins.enableDevLogging();
         });
 
-        app.get("/users", ctx -> ctx.result("GET /users"));
-        app.post("/users", ctx -> ctx.result("POST /users"));
-
         app.get("/hello", ctx -> {
             var name = ctx.queryParamAsClass("name", String.class).getOrDefault("World");
             ctx.result("Hello, " + name + "!");
         });
 
+        app.get("/courses/build", ctx -> {
+
+        });
+
+        app.get("/courses/{id}", ctx -> {
+            ctx.result("Course ID: " + ctx.pathParam("id"));
+        });
+
+        app.get("/courses/{courseId}/lessons/{id}", ctx -> {
+            var courseId = ctx.pathParam("courseId");
+            var lessonId = ctx.pathParam("id");
+            ctx.result("Course ID: " + courseId + " Lesson ID: " + lessonId);
+        });
+
+        app.get("/users/{id}", ctx -> {
+            ctx.result("User ID: " + ctx.pathParam("id"));
+        });
+
+        app.get("/users/{id}/post/{postId}", ctx -> {
+            var userId = ctx.pathParam("id");
+            var postId = ctx.pathParam("postId");
+            ctx.result("User ID: " + userId + " Post ID: " + postId);
+        });
+
         app.start(7070);
     }
+
+//    public static void show(Context ctx) {
+//        var id = ctx.pathParamAsClass("id", Long.class).get();
+//        var user = UserRepository.find(id)
+//                .orElseThrow(() -> new NotFoundResponse("Entity with id = " + id + " not found"));
+//    }
 }
 
